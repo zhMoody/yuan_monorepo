a1698481355c6cbe2a8ff877d015dc3f20aa00ae
Moody <2693131889@qq.com>
2023-02-17 19:40:29 +0800

    fate: 新增启动loading页面，优化左边导航动画， 添加博客信息，更改docker文件，更新登录失败弹窗

--- src/views/Acg/index.vue
+++ src/views/Acg/index.vue
@@ -1,25 +1,64 @@
 <template>
-  <div class="text">当前模拟数据量:{{ state.DataList.length }}条</div>
-  <div class="box">
-    <div ref="scrollBox" class="container" @scroll="doscroll">
-      <div ref="items">
-        <div class="item" v-for="item in virtualList" :key="item.tid">
-          <img v-lazy="item.src" alt=""/>
-          <span>{{ item.text }}</span>
-        </div>
+  <!--  <div class="text">当前模拟数据量:{{ state.DataList.length }}条</div>-->
+  <!--  <div class="box">-->
+  <!--    <div ref="scrollBox" class="container" @scroll="doscroll">-->
+  <!--      <div ref="items">-->
+  <!--        <div class="item" v-for="item in virtualList" :key="item.tid">-->
+  <!--          <img v-lazy="item.src" alt=""/>-->
+  <!--          <span>{{ item.text }}</span>-->
+  <!--        </div>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <code class="code">{{ virtualList }}</code>-->
+  <!--  </div>-->
+  <div class="content">
+    <div class="qrcode">
+      <div ref="shellContainerRef" class="qrcodeBox">
+        <QrcodeVue :size="size" :value="value" background="#0acf97" level="H"/>
+        <div>手机端体验</div>
+      </div>
+      <div>
+        <NButton disabled="true" @click="handleClick">下载</NButton>
       </div>
     </div>
-    <code class="code">{{ virtualList }}</code>
+    <div>
+      <img :src="imgUel" alt="">
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+
 import {arr} from './testData'
 import {reactive, ref, computed, watchEffect, nextTick} from "vue";
+import html2canvas from 'html2canvas'
+import QrcodeVue from 'qrcode.vue'
+import {NButton} from 'naive-ui'
+
+const value = ref<string>("http://36.133.74.165:9000/")
+const size = ref<string>('200')
+const shellContainerRef = ref<HTMLDivElement | null>()
+const imgUel = ref<string>('')
+const handleClick = () => {
+  if (shellContainerRef.value) {
+    html2canvas(shellContainerRef.value, {
+      width: shellContainerRef.value.clientWidth, //dom 原始宽度
+      height: shellContainerRef.value.clientHeight,
+      scrollY: 0, // html2canvas默认绘制视图内的页面，需要把scrollY，scrollX设置为0
+      scrollX: 0,
+      useCORS: true, //支持跨域，但好像没什么用
+      allowTaint: true, //允许跨域（默认false）
+      scale: 1
+    }).then((canvas) => {
+      // 生成的ba64图片
+      const base64Data = canvas.toDataURL('image/jpeg', 1)
+      imgUel.value = base64Data
+    })
+  }
+}
 
 const scrollBox = ref<null | HTMLDivElement>(null);
 const items = ref<null | HTMLDivElement>(null);
-
 const state = reactive({
   DataList: arr,
   ItemBoxHeight: 0,
@@ -64,7 +103,41 @@ watchEffect(() => {
 });
 </script>
 
-<style scoped lang="less">
+<style lang="less" scoped>
+.content {
+  min-height: calc(100vh - 120px);
+  overflow: hidden;
+  border-radius: 2px;
+  box-shadow: 0 1px 3px rgb(0 0 0 / 5%);
+  background: var(--c-f1f3f4);
+
+  .qrcode {
+    width: 300px;
+    height: 470px;
+    background: #0acf97;
+    border-radius: 5px;
+    margin: 300px auto;
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
+
+    .qrcodeBox {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 10px;
+      width: 300px;
+      height: 400px;
+      margin: 0 auto;
+      background: #0acf97;
+      font-size: 30px;
+    }
+  }
+}
+
 .text {
   width: 500px;
   font-size: 36px;
@@ -109,7 +182,7 @@ watchEffect(() => {
   justify-content: center;
   gap: 10px;
   align-items: center;
-  
+
 }
 
 .container .item img {
